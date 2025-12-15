9829f8a1f0ade498ad95321baf6d978f8aaae137
kevincalixte <kevin.calixte@icloud.com>
2025-12-15 21:12:25 +0100

    dashboard: tasks color

--- dashboard/components/Dashboard/Tasks.tsx
+++ dashboard/components/Dashboard/Tasks.tsx
@@ -5,10 +5,10 @@ import { IconType } from 'react-icons'
 export const Tasks = () => {
     return (
         <div className='grid grid-cols-[1fr_1fr_1fr_1fr] gap-5 p-5 '>
-            <TaskCard Icon={FiAlertCircle} number={2} text='urgent tasks' color={['red-500', 'red-400']} />
-            <TaskCard Icon={FiActivity} number={4} text='ongoing tasks' color={['orange-500', 'orange-400']} />
-            <TaskCard Icon={FiCalendar} number={11} text='scheduled tasks' color={['blue-500', 'blue-400']} />
-            <TaskCard Icon={FiCheckCircle} number={5} text='completed tasks' color={['green-500', 'green-400']} />
+            <TaskCard Icon={FiAlertCircle} number={2} text='urgent tasks' color={['from-red-500', 'to-red-400']} />
+            <TaskCard Icon={FiActivity} number={4} text='ongoing tasks' color={['from-orange-500', 'to-orange-400']} />
+            <TaskCard Icon={FiCalendar} number={11} text='scheduled tasks' color={['from-blue-500', 'to-blue-400']} />
+            <TaskCard Icon={FiCheckCircle} number={5} text='completed tasks' color={['from-green-500', 'to-green-400']} />
         </div>
     )
 }
@@ -26,7 +26,7 @@ const TaskCard = ({
 }) => {
     return (
 
-        <div className={`bg-linear-to-r from-${color[0]} to-${color[1]} shadow-lg cursor-pointer text-white rounded-sm h-10 flex items-center justify-center gap-2 hover:scale-102 transition ease-in-out `}>
+        <div className={`bg-linear-to-r ${color[0]} ${color[1]} shadow-lg cursor-pointer text-white rounded-sm h-10 flex items-center justify-center gap-2 hover:scale-102 transition ease-in-out `}>
             <Icon />
             <span className='font-bold text-lg'>{number}</span>
             <span>{text}</span>

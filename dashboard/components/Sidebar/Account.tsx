ced626b59f17821ff3957de6489087c01ab189e0
kevincalixte <kevin.calixte@icloud.com>
2025-12-15 21:33:04 +0100

    dashboard: account ts update

--- dashboard/components/Sidebar/Account.tsx
+++ dashboard/components/Sidebar/Account.tsx
@@ -3,16 +3,28 @@ import React from 'react'
 export const Account = () => {
     return (
         <div className='border-b border-stone-300 mb-4 mt-2 pb-4'>
-            <button className='flex p-0.5 hover:bg-stone-200 rounded transition-colors relative gap-2 w-full items-center'>
-                <img className='size-8 rounded shadow ' src="https://api.dicebear.com/9.x/initials/svg?seed=Kevin&scale=80" alt="" />
-           
-            <div className='text-start'> 
-                <span className='text-sm block font-semibold'>Kevin Calixte</span>
-                <span className='text-xs block'>kevin.calixte@ceppic.fr</span>
-            </div>
-           
-            </button>
-
+            <AccountCard avatar='https://api.dicebear.com/9.x/initials/svg?seed=KC&scale=80' name='Kevin Calixte' email='kevin.calixte@ceppic.fr' />
         </div>
     )
+}
+
+const AccountCard = ({
+    avatar,
+    name,
+    email
+}: {
+    avatar: string;
+    name: string;
+    email: string;
+}) => {
+    return (
+        <button className='flex p-0.5 hover:bg-stone-200 rounded transition-colors relative gap-2 w-full items-center'>
+            <img className='size-8 rounded shadow ' src={avatar} alt="" />
+            <div className='text-start'>
+                <span className='text-sm block font-semibold'>{name}</span>
+                <span className='text-xs block'>{email}</span>
+            </div>
+        </button>
+    )
+
 }

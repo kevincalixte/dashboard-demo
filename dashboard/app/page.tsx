0f9ce8a7f29e4e714157aef7902e1770b1e5c7bf
kevincalixte <kevin.calixte@icloud.com>
2025-12-15 20:54:19 +0100

    dashboard: tasks added

--- dashboard/app/page.tsx
+++ dashboard/app/page.tsx
@@ -1,9 +1,9 @@
 import { Dashboard } from "@/components/Dashboard/Dashboard";
 import { Sidebar } from "@/components/Sidebar/Sidebar";
-// https://api.dicebear.com/7.x/avataaars/svg?seed=Kevin"
+
 export default function Home() {
   return (
-    <main className="grid gap-4 p-4 grid-cols-[240px_1fr]">
+    <main className="grid gap-4 p-4 grid-cols-[240px_1fr] ml-3 ">
       <Sidebar />
       <Dashboard />
     </main>

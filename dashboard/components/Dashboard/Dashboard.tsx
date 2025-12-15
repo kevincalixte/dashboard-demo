367810093a7603711cc5dddeca1b82921e5a74cd
kevincalixte <kevin.calixte@icloud.com>
2025-12-15 03:13:32 +0100

    init

--- dashboard/components/Dashboard/Dashboard.tsx
+++ dashboard/components/Dashboard/Dashboard.tsx
@@ -2,8 +2,7 @@ import React from 'react'
 
 export const Dashboard = () => {
   return (
-    <div className='bg-white rounded-lg shadow pb-4 h-screen'>
-        
+    <div className='bg-white rounded-lg shadow pb-4 h-screen'>    
     </div>
   )
 }

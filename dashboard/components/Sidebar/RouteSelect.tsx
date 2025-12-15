e34a2d1f392109cdb839f78e4045dd3a4a4680a6
kevincalixte <kevin.calixte@icloud.com>
2025-12-15 21:45:32 +0100

    dashboard: RouteSelect +

--- dashboard/components/Sidebar/RouteSelect.tsx
+++ dashboard/components/Sidebar/RouteSelect.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { IconType } from 'react-icons'
-import { FaBus, FaCalendar, FaHome, FaUsers, FaUserTie } from 'react-icons/fa'
+import { FaBus, FaCalendar, FaClipboardList, FaEnvelope, FaHome, FaTasks, FaUsers, FaUserTie } from 'react-icons/fa'
 
 export const RouteSelect = () => {
     return (
@@ -9,7 +9,10 @@ export const RouteSelect = () => {
             <Route selected={false} Icon={FaUserTie} title="Guides" />
             <Route selected={false} Icon={FaBus} title="Drivers" />
             <Route selected={false} Icon={FaUsers} title="Clients" />
+            <Route selected={false} Icon={FaEnvelope} title="Messages" />
             <Route selected={false} Icon={FaCalendar} title="Agenda" />
+            <Route selected={false} Icon={FaClipboardList} title="Reservations" />
+            <Route selected={false} Icon={FaTasks} title="Tasks" />
         </div>
     )
 }
@@ -24,10 +27,10 @@ const Route = ({
     title: string;
 }) => {
     return (
-        <button className={`flex items-center justify-start gap-2 w-full rounded px-2 py-1.5 text-sm transition-[box-shadow,background-color,color]
+        <button className={`flex items-center justify-start gap-2 w-full rounded px-2 py-1.5 text-md transition-[box-shadow,background-color,color]
             ${selected
                 ? 'bg-white text-stone-950 shadow'
-                : 'hover:bg-stone-200 bg-transparent text-stone-500 shadow-none'}`}>
+                : 'hover:bg-stone-200 hover:text-stone-700 text-stone-500 shadow-none'}`}>
             <Icon />
             <span>{title}</span>
         </button>
--- dashboard/components/Sidebar/Search.tsx
+++ dashboard/components/Sidebar/Search.tsx
@@ -3,7 +3,7 @@ import { FiBarChart, FiCommand, FiSearch } from 'react-icons/fi'
 
 export const Search = () => {
     return (
-        <div className='bg-stone-200 mb-4 relative rounded flex items-center px-2 py-1.5 text-sm'>
+        <div className='bg-stone-200 mb-4 relative rounded flex items-center px-2 py-1.5 text-md'>
             <FiSearch className='mr-2' />
             <input type="text" placeholder='Search' className='w-full bg-transparent placeholder:text-stone-400 focus:outline-none' />
         </div>
